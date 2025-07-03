1a15b5cb1a51e3833b0b00c6b774f0058a7c3a2e
maaz-links <mzit3116@gmail.com>
2025-07-03 17:03:12 +0500

    faq

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -1,8 +1,6 @@
 import { Accordion, AccordionItem as Item } from "@szhsin/react-accordion";
 import { useEffect, useState } from "react";
 import axiosClient from "../../axios-client";
-import { useEffect, useState } from "react";
-import axiosClient from "../../axios-client";
 
 /**
  * @type {React.ExoticComponent<import('@szhsin/react-accordion').AccordionItemProps>}
@@ -48,8 +46,6 @@ const AccordionItem = ({ header, ...rest }) => (
     panelProps={{ className: "flex text-gray-400 w-full items-center text-left" }}
   />
 );
-
-
 
 
 
