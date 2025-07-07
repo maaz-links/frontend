b789e9201db0074ab9f27b86295b637531d23174
Azhar-lone <azharlonem@gmail.com>
2025-07-07 18:46:30 +0500

    adjusted width for slider panel

--- src/pages/search/FilterPanel.jsx
+++ src/pages/search/FilterPanel.jsx
@@ -24,7 +24,7 @@ function FilterPanel({ isOpen, onClose, filters, setFilters }) {
 
       {/* Filter Panel */}
       <div
-        className={`fixed top-0 right-0 h-full rounded-3xl w-80 bg-white shadow-lg z-50 transform transition-transform duration-300 ease-in-out ${
+        className={`fixed top-0 right-0 h-full rounded-3xl w-[80%] md:w-[484px] bg-white shadow-lg z-50 transform transition-transform duration-300 ease-in-out ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
