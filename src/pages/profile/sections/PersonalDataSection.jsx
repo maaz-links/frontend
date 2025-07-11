86181e1625e1c001465a51ab31dfa66a056c3b9b
Azhar-lone <azharlonem@gmail.com>
2025-07-11 20:11:17 +0500

    improvements in responsivenss

--- src/pages/profile/sections/PersonalDataSection.jsx
+++ src/pages/profile/sections/PersonalDataSection.jsx
@@ -43,31 +43,31 @@ export default function PersonalDataSection({ personalData }) {
         </div>
 
         {/* Date of Birth */}
-        <div className=" flex  flex-wrap  ">
+        <div className="space-y-3   ">
           <label className="text-base  font-bold text-[#090909] tracking-[-0.03em]">
             Date of birth
           </label>
-          <div className="flex gap-[18px]">
+          <div className="flex gap-3 md:gap-[18px] flex-wrap ">
             <input
               type="text"
               value={personalData.dateOfBirth.day}
               readOnly
               placeholder={personalData.dateOfBirth.day}
-              className="w-[133px] h-[55px] border border-[rgba(12,16,56,0.22)] rounded-xl px-6 py-4 text-base font-medium text-black/80 tracking-[-0.03em] backdrop-blur-[12.5px]"
+              className="md:w-[133px]  w-24 h-[55px] border border-[rgba(12,16,56,0.22)] rounded-xl px-6 py-4 text-base font-medium text-black/80 tracking-[-0.03em] backdrop-blur-[12.5px]"
             />
             <input
               type="text"
               value={personalData.dateOfBirth.month}
               readOnly
               placeholder={personalData.dateOfBirth.month}
-              className="w-[133px] h-[55px] border border-[rgba(12,16,56,0.22)] rounded-xl px-6 py-4 text-base font-bold text-black/80 tracking-[-0.03em] backdrop-blur-[12.5px]"
+              className="md:w-[133px] w-24 h-[55px] border border-[rgba(12,16,56,0.22)] rounded-xl px-6 py-4 text-base font-bold text-black/80 tracking-[-0.03em] backdrop-blur-[12.5px]"
             />
             <input
               type="text"
               value={personalData.dateOfBirth.year}
               readOnly
               placeholder={personalData.dateOfBirth.year}
-              className="w-[133px] h-[55px] border border-[rgba(12,16,56,0.22)] rounded-xl px-6 py-4 text-base font-medium text-black/80 tracking-[-0.03em] backdrop-blur-[12.5px]"
+              className="w-24 md:w-[133px] h-[55px] border border-[rgba(12,16,56,0.22)] rounded-xl px-6 py-4 text-base font-medium text-black/80 tracking-[-0.03em] backdrop-blur-[12.5px]"
             />
           </div>
         </div>
--- src/pages/profile/sections/ProfileBanner.jsx
+++ src/pages/profile/sections/ProfileBanner.jsx
@@ -4,17 +4,11 @@ import { X } from "lucide-react";
 
 export default function ProfileBanner({ isComplete, onToggleProfile }) {
   return (
-    <div className="relative w-full  mx-auto h-44 md:h-[125px] bg-[#8880FE] rounded-[20px] mt-2 overflow-hidden">
-      {/* Decorative Stars */}
-      <div className="absolute w-20 h-20 bg-white/25 rounded-full top-[25px] left-[664px]"></div>
-      <div className="absolute w-20 h-20 bg-white/10 rounded-full top-[73px] left-[734px]"></div>
-      <div className="absolute w-20 h-[118px] bg-white/10 rounded-full top-[102px] left-[593px]"></div>
-      <div className="absolute w-[61px] h-[118px] bg-white/10 rounded-full top-[-79px] left-[734px]"></div>
-      <div className="absolute w-[61px] h-[60px] bg-white/10 rounded-full top-0 left-[603px]"></div>
+    <div className="  w-[98%]     mx-auto h-fit md:h-[125px] bg-[#8880FE] rounded-[20px] mt-2 ">
 
       {/* Content */}
-      <div className="flex flex-col md:flex-row">
-        <div className="absolute left-10 top-1/2 -translate-y-1/2 flex  flex-col gap-1">
+      <div className="flex flex-col md:flex-row justify-between px-4 gap-5 md:text-start text-center py-10 md:px-16 items-center h-full">
+        <div className="  flex  flex-col gap-3">
           <h2 className="text-[24px] font-bold  text-white  leading-[24px]">
             {isComplete
               ? "Your profile is 50% complete"
@@ -30,7 +24,7 @@ export default function ProfileBanner({ isComplete, onToggleProfile }) {
         {/* Button */}
         <button
           onClick={onToggleProfile}
-          className="absolute md:right-[40px] hover:bg-[#8880FE] hover:border-white hover:border md:top-1/2 md:-translate-y-1/2 bg-[#090909] rounded-xl px-7 py-3 h-[60px] flex items-center justify-center"
+          className=" md:right-[40px] hover:bg-[#8880FE] hover:border-white hover:border  bg-[#090909] rounded-xl px-7 py-3 h-[60px] flex items-center justify-center"
         >
           <span className="text-white  text-base font-normal tracking-[-0.03em] capitalize">
             {isComplete ? "Finish Your Profile" : "Fill Out My Profile"}
@@ -38,8 +32,8 @@ export default function ProfileBanner({ isComplete, onToggleProfile }) {
         </button>
       </div>
       {/* Close Button */}
-      <button className="absolute right-4 top-3 opacity-30">
-        <X className="w-[18px] h-[18px] text-white" strokeWidth={2} />
+      <button className="absolute right-10 top-3 opacity-50">
+        <X className=" text-white" strokeWidth={2} />
       </button>
     </div>
   );
